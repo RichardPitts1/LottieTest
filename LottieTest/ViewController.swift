bf3be19e67ef1f75472ad88f67648d508703ed33
Richard E Pitts <RichardRPitts@Richard-E-Pittss-Mac.local>
2017-07-31 10:43:09 -0500

    Initial Commit

---
+++ LottieTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LottieTest
+//
+//  Created by Richard E Pitts on 7/31/17.
+//  Copyright © 2017 Richard E Pitts. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
